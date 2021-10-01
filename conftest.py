5e27acf78c5aa8cc3c5801489ccf8316c2e41d9c
VeronicaKo <VeronicaKo1987@gmail.ru>
2021-10-01 11:19:00 +0300

    own file for each page

--- conftest.py
+++ conftest.py
@@ -14,11 +14,12 @@ def browser():
 
 @pytest.fixture(scope="function")
 def login_as_user(browser):
-    login_as_user = LogInPage(browser)
-    login_as_user.go_to_site()
-    login_as_user.enter_login(config.login)
-    login_as_user.enter_domain(config.domain)
-    login_as_user.click_on_the_password_button()
-    login_as_user.enter_password(config.password)
-    login_as_user.click_on_the_enter_button()
+    login_page = LogInPage(browser)
+    login_page.go_to_site()
+    login_page.enter_login(config.login)
+    login_page.enter_domain(config.domain)
+    login_page.click_on_the_password_button()
+    login_page.enter_password(config.password)
+    login_page.click_on_the_enter_button()
+    assert login_page.checking_move_on_letter_page(), 'The login to the email is not completed'
     return AddLetterPage(browser)
