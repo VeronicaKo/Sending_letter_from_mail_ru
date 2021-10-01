6ed2616cf620c35c2f4da3153ebd12ad3a6814ee
VeronicaKo <VeronicaKo1987@gmail.ru>
2021-10-01 10:10:27 +0300

    added new file for password and login

---
+++ config.py
@@ -0,0 +1,4 @@
+login = "quality_lab_test"
+domain = "@list.ru"
+password = "TA3OD3yporo%"
+recipient = "quality_lab_test@list.ru"
--- tests/test_send_new_letter.py
+++ tests/test_send_new_letter.py
@@ -1,31 +1,33 @@
-from pages.MailPages import AddLetter
-from pages.MailPages import LogIn
+from pages.MailPages import AddLetterPage
+from pages.MailPages import LogInPage
+
+import config
 
 
 def test_log_in(browser):
-    current_page = LogIn(browser)
+    current_page = LogInPage(browser)
     current_page.go_to_site()
-    current_page.enter_login("quality_lab_test")
-    current_page.enter_domain("@list.ru")
+    current_page.enter_login(config.login)
+    current_page.enter_domain(config.domain)
     current_page.click_on_the_password_button()
-    current_page.enter_password("TA3OD3yporo%")
+    current_page.enter_password(config.password)
     current_page.click_on_the_enter_button()
     assert current_page.checking_move_on_letter_page(), 'The login to the email is not completed'
 
 
 def test_add_letter(browser):
-    current_page = LogIn(browser)
+    current_page = LogInPage(browser)
     current_page.go_to_site()
-    current_page.enter_login("quality_lab_test")
-    current_page.enter_domain("@list.ru")
+    current_page.enter_login(config.login)
+    current_page.enter_domain(config.domain)
     current_page.click_on_the_password_button()
-    current_page.enter_password("TA3OD3yporo%")
+    current_page.enter_password(config.password)
     current_page.click_on_the_enter_button()
     assert current_page.checking_move_on_letter_page(), 'The login to the email is not completed'
 
-    letter_page = AddLetter(browser)
+    letter_page = AddLetterPage(browser)
     letter_page.go_to_letter_site()
-    letter_page.enter_email('quality_lab_test@list.ru')
+    letter_page.enter_email(config.recipient)
     letter_page.enter_subject('English proverbs')
     letter_page.enter_text_of_letter(
         'A cat in gloves catches no mice. Many hands make light work. An apple a day keeps the doctor away.')
